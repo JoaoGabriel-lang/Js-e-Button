ef2f830da17066d067979eac2b9b2193b8c53d06
JoaoGabriel-lang <jgabriel.rochaalves@gmail.com>
2024-09-28 11:55:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function salvarDados(){
+
+let nome = prompt("Coloca o nome ai fera")
+
+let idade = prompt("Coloca a idade ai meu colega")
+
+alert('Então vc é o tal '+ nome + " com a idade de " + idade )
+}
